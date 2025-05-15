35259f7f6d23375bd896c4112fe2fb50a6bd7c49
Layslla Eduarda Oreti dos Santos <116527092+LaysllaOreti@users.noreply.github.com>
2025-05-15 08:00:58 -0300

    Add files via upload
    Adicionando exercícios de Python

---
+++ Aula 14.05/Exercicio_2.py
@@ -0,0 +1,7 @@
+numeros = [2, 5, 7, 9]
+
+for numero in numeros:
+    soma = sum(numeros) #Somo todos os números da lista
+    media = soma /len(numeros) #Divido a soma pelo tamanho da lista
+    print(media)
+
---
+++ Aula While 14.05/Exemplos_Aula.py
@@ -0,0 +1,33 @@
+"""
+Exemplo 1
+i = 1
+while i <= 5:
+    print(i)
+    i += 1
+"""
+
+"""
+Exemplo 2
+x = True
+while x:
+    print('Estou rodando')
+"""
+
+"""
+Exemplo 3
+pedido = ''
+while pedido != 'quero sair':
+    pedido = input('Você não vai sair: ')
+"""
+
+"""
+Exemplo 4
+contador = 0
+while contador < 9:
+    print(contador, end = ' ')
+    contador = contador + 1
+
+    if contador == 5:
+        break
+"""
+
